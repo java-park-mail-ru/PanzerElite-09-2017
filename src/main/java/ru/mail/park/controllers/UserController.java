da7ae8e97934702d1a0680436795c86578977487
Crusader727 <rubenhov97@gmail.com>
2017-09-20 20:50:26 +0300

    codestyle

--- src/main/java/ru/mail/park/controllers/UserController.java
+++ src/main/java/ru/mail/park/controllers/UserController.java
@@ -2,13 +2,15 @@ package ru.mail.park.controllers;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import ru.mail.park.helpers.Constants;
 import ru.mail.park.models.User;
 import ru.mail.park.services.UserService;
 import org.springframework.http.HttpStatus;
+
 import javax.servlet.http.HttpSession;
 
 @RestController
-@CrossOrigin(origins = "*", maxAge = 3600)
+@CrossOrigin(origins = "*", maxAge = Constants.CROSS_ORIGIN_MAX_AGE)
 @RequestMapping("/api/user")
 public class UserController {
 
@@ -19,53 +21,53 @@ public class UserController {
     @RequestMapping(path = "/login", method = RequestMethod.POST, produces = "application/json", consumes = "application/json")
     public ResponseEntity<String> loginUser(@RequestBody User body, HttpSession httpSession) {
         final ResponseEntity<String> res = new ResponseEntity<>(userService.login(body));
-        setHttpSession( res, httpSession, body);
+        setHttpSession(res, httpSession, body);
         return res;
     }
 
     @RequestMapping(path = "/register", method = RequestMethod.POST, produces = "application/json", consumes = "application/json")
     public ResponseEntity<String> registerUser(@RequestBody User body, HttpSession httpSession) {
-        final ResponseEntity<String> res = new ResponseEntity<>( userService.register(body));
+        final ResponseEntity<String> res = new ResponseEntity<>(userService.register(body));
         setHttpSession(res, httpSession, body);
         return res;
     }
 
     @RequestMapping(path = "/getuser", method = RequestMethod.GET, produces = "application/json")
-    public ResponseEntity<String> getUser( HttpSession httpSession) {
+    public ResponseEntity<String> getUser(HttpSession httpSession) {
         final ResponseEntity<String> answer;
-        final User user = ( User) httpSession.getAttribute(SESSIONKEY);
-        if( user != null) {
-            answer = new ResponseEntity<>( user.getUser().toString(), HttpStatus.OK);
+        final User user = (User) httpSession.getAttribute(SESSIONKEY);
+        if (user != null) {
+            answer = new ResponseEntity<>(user.getUser().toString(), HttpStatus.OK);
         } else {
-            answer = new ResponseEntity<>( HttpStatus.UNAUTHORIZED);
+            answer = new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
         }
         return answer;
     }
 
     @RequestMapping(path = "/logout", method = RequestMethod.GET, produces = "application/json")
-    public ResponseEntity<String> logoutUser( HttpSession httpSession) {
+    public ResponseEntity<String> logoutUser(HttpSession httpSession) {
         final ResponseEntity<String> answer;
-        if( httpSession.getAttribute(SESSIONKEY) != null) {
+        if (httpSession.getAttribute(SESSIONKEY) != null) {
             httpSession.removeAttribute(SESSIONKEY);
-            answer = new ResponseEntity<>( HttpStatus.OK);
+            answer = new ResponseEntity<>(HttpStatus.OK);
         } else {
-            answer = new ResponseEntity<>( HttpStatus.BAD_REQUEST);
+            answer = new ResponseEntity<>(HttpStatus.BAD_REQUEST);
         }
         return answer;
     }
 
     @RequestMapping(path = "/changepassword", method = RequestMethod.POST, produces = "application/json", consumes = "application/json")
     public ResponseEntity<String> changePasswordUser(@RequestBody User body, HttpSession httpSession) {
-        final User user = (User) httpSession.getAttribute( SESSIONKEY);
-        body.setLogin( user.getLogin());
-        final ResponseEntity<String> res = new ResponseEntity<>( userService.changePassword(body));
-        setHttpSession( res, httpSession, body);
+        final User user = (User) httpSession.getAttribute(SESSIONKEY);
+        body.setLogin(user.getLogin());
+        final ResponseEntity<String> res = new ResponseEntity<>(userService.changePassword(body));
+        setHttpSession(res, httpSession, body);
         return res;
     }
 
-    public void setHttpSession( ResponseEntity<String> res, HttpSession httpSession, User body) {
-        if( res.getStatusCode() == HttpStatus.OK) {
-            httpSession.setAttribute( SESSIONKEY, body);
+    public void setHttpSession(ResponseEntity<String> res, HttpSession httpSession, User body) {
+        if (res.getStatusCode() == HttpStatus.OK) {
+            httpSession.setAttribute(SESSIONKEY, body);
         }
     }
 }
---
+++ src/main/java/ru/mail/park/helpers/Constants.java
@@ -0,0 +1,6 @@
+package ru.mail.park.helpers;
+
+public class Constants {
+
+    public static final Integer CROSS_ORIGIN_MAX_AGE = 3600;
+}
