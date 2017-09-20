5816559621b6ce713d267602186881762e3af6c3
Crusader727 <rubenhov97@gmail.com>
2017-09-20 17:42:24 +0300

    first commit

---
+++ src/main/java/ru/mail/park/services/UserService.java
@@ -0,0 +1,49 @@
+package ru.mail.park.services;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import ru.mail.park.models.User;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicLong;
+
+@Service
+public class UserService {
+    private Map<String, User> db;
+    private AtomicLong idgen;
+
+    public UserService() {
+        db = new HashMap<>();
+        idgen = new AtomicLong(0);
+    }
+
+    public HttpStatus login(User user) {
+        final String login = user.getLogin();
+        final String password = user.getPassword();
+        if (db.get(login) != null && db.get(login).getPassword().equals(password)) {
+            return HttpStatus.OK;
+        }
+        else {
+            return HttpStatus.NOT_FOUND;
+        }
+    }
+    public HttpStatus register(User user) {
+        user.setId(String.valueOf(idgen.getAndIncrement()));
+        if (db.put(user.getLogin(), user) == null) {
+            return HttpStatus.OK ;
+        } else {
+            idgen.getAndDecrement();
+            return HttpStatus.FORBIDDEN;
+        }
+    }
+    public HttpStatus changePassword(User user) {
+        final String password = user.getPassword();
+        if (db.get(user.getLogin()) != null) {
+            db.get(user.getLogin()).setPassword(password);
+            return HttpStatus.OK ;
+        } else {
+            return HttpStatus.FORBIDDEN;
+        }
+    }
+
+}
