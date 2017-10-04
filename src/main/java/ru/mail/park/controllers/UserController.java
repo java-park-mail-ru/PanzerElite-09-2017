de7ace5f960151785235efe26ffac19398f1bbc2
Kurkin Nickolay <mrnickrus@gmail.com>
2017-10-04 17:50:12 +0300

    rk1 changes

---
+++ src/main/java/ru/mail/park/controllers/UserController.java
@@ -0,0 +1,73 @@
+package ru.mail.park.controllers;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import ru.mail.park.models.User;
+import ru.mail.park.services.UserService;
+import org.springframework.http.HttpStatus;
+
+import javax.servlet.http.HttpSession;
+
+@RestController
+@CrossOrigin(origins = "*")
+@RequestMapping("/api/user")
+public class UserController {
+
+    private UserService userService = new UserService();
+
+    private static final String SESSIONKEY = "user";
+
+    @RequestMapping(path = "/login", method = RequestMethod.POST, produces = "application/json", consumes = "application/json")
+    public ResponseEntity<String> loginUser(@RequestBody User body, HttpSession httpSession) {
+        final ResponseEntity<String> res = new ResponseEntity<>(userService.login(body));
+        setHttpSession(res, httpSession, body);
+        return res;
+    }
+
+    @RequestMapping(path = "/register", method = RequestMethod.POST, produces = "application/json", consumes = "application/json")
+    public ResponseEntity<String> registerUser(@RequestBody User body, HttpSession httpSession) {
+        final ResponseEntity<String> res = new ResponseEntity<>(userService.register(body));
+        setHttpSession(res, httpSession, body);
+        return res;
+    }
+
+    @RequestMapping(path = "/getuser", method = RequestMethod.GET, produces = "application/json")
+    public ResponseEntity<String> getUser(HttpSession httpSession) {
+        final ResponseEntity<String> answer;
+        final User user = (User) httpSession.getAttribute(SESSIONKEY);
+        if (user != null) {
+            answer = new ResponseEntity<>(user.getUser().toString(), HttpStatus.OK);
+        } else {
+            answer = new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+        }
+        return answer;
+    }
+
+    @RequestMapping(path = "/logout", method = RequestMethod.GET, produces = "application/json")
+    public ResponseEntity<String> logoutUser(HttpSession httpSession) {
+        final ResponseEntity<String> answer;
+        if (httpSession.getAttribute(SESSIONKEY) != null) {
+            //httpSession.removeAttribute(SESSIONKEY);
+            httpSession.invalidate();
+            answer = new ResponseEntity<>(HttpStatus.OK);
+        } else {
+            answer = new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+        return answer;
+    }
+
+    @RequestMapping(path = "/changepassword", method = RequestMethod.POST, produces = "application/json", consumes = "application/json")
+    public ResponseEntity<String> changePasswordUser(@RequestBody User body, HttpSession httpSession) {
+        final User user = (User) httpSession.getAttribute(SESSIONKEY);
+        body.setLogin(user.getLogin());
+        final ResponseEntity<String> res = new ResponseEntity<>(userService.changePassword(body));
+        setHttpSession(res, httpSession, body);
+        return res;
+    }
+
+    public void setHttpSession(ResponseEntity<String> res, HttpSession httpSession, User body) {
+        if (res.getStatusCode() == HttpStatus.OK) {
+            httpSession.setAttribute(SESSIONKEY, body);
+        }
+    }
+}
