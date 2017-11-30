f3c9ed23b308cb018323e29fc50c172e4c935c80
Crusader727 <rubenhov97@gmail.com>
2017-11-30 15:26:32 +0300

    for heroku

--- src/main/java/ru/mail/park/utils/Constants.java
+++ src/main/java/ru/mail/park/utils/Constants.java
@@ -2,7 +2,7 @@ package ru.mail.park.utils;
 
 public class Constants {
     public static final String[] TRUSTED_URLS = new String[]{
-            "https://panzerelitefront.herokuapp.com",
+            "https://panzerelite.herokuapp.com",
             "http://localhost:8080",
     };
     public static final String SESSION_EXTRA_USER = "user";
