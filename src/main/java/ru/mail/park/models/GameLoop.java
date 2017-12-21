adbf17534eb5c5f63a94ad16ad218f08a09d3935
Crusader727 <rubenhov97@gmail.com>
2017-12-21 18:57:05 +0300

    for pr

--- src/main/java/ru/mail/park/models/GameLoop.java
+++ src/main/java/ru/mail/park/models/GameLoop.java
@@ -1,6 +1,9 @@
 package ru.mail.park.models;
 
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.time.Clock;
 import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
@@ -9,6 +12,7 @@ public class GameLoop implements Runnable {
     private static final long STEP_TIME = 40;
 
     private final Room gameMechanics;
+    private static final Logger log = LoggerFactory.getLogger(GameLoop.class);
 
 
     private Clock clock = Clock.systemDefaultZone();
@@ -51,12 +55,15 @@ public class GameLoop implements Runnable {
                     final long sleepingTime = Math.max(0, STEP_TIME - (after - before));
                     Thread.sleep(sleepingTime);
                 } catch (InterruptedException e) {
+                    log.warn("interupted");
                     break;
                 }
                 if (Thread.currentThread().isInterrupted()) {
+                    log.warn("interupted");
                     break;
                 }
             } catch (RuntimeException e) {
+                log.warn("gamemechanics failed");
                 break;
             }
         }
